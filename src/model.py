171d40033bb1437c8d73410ef557d1e95130a77d
Olubunmi-Amoke <109986627+Olubunmi-Amoke@users.noreply.github.com>
2024-08-24 21:54:08 -0500

    Add files via upload
    first commit

---
+++ src/model.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# define the CNN architecture
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+
+        super(MyModel, self).__init__()
+
+        # Define a CNN architecture        
+        self.model = nn.Sequential(
+            
+            #First Convolution Block
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(p=dropout),
+            
+            #Second Convolution Block
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            
+            #Third Convolution Block
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(p=dropout),
+            
+            #Fourth Convolution Block
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            
+            nn.Flatten(),
+            
+            nn.Linear(128 * 14 * 14, 500),
+            nn.Dropout(p=dropout),
+            nn.BatchNorm1d(500),
+            nn.ReLU(),
+            nn.Linear(500, num_classes)
+        )
+        
+        
+    def forward(self, x):
+        x = self.model(x)
+
+        return x
+
+######################################################################################
+#                                     TESTS
+######################################################################################
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    from .data import get_data_loaders
+
+    return get_data_loaders(batch_size=2)
+
+
+def test_model_construction(data_loaders):
+
+    model = MyModel(num_classes=23, dropout=0.3)
+
+    dataiter = iter(data_loaders["train"])
+    images, labels = dataiter.next()
+
+    out = model(images)
+
+    assert isinstance(
+        out, torch.Tensor
+    ), "The output of the .forward method should be a Tensor of size ([batch_size], [n_classes])"
+
+    assert out.shape == torch.Size(
+        [2, 23]
+    ), f"Expected an output tensor of size (2, 23), got {out.shape}"
